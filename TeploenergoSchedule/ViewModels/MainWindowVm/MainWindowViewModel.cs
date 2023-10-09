da2d393f5ddf494010ed5e9e0492a5b4aac155c4
DadonovAleksandr <a.snafu@ya.ru>
2023-10-09 10:17:51 +0300

    Загрузка списка файлов

--- TeploenergoSchedule/ViewModels/MainWindowVm/MainWindowViewModel.cs
+++ TeploenergoSchedule/ViewModels/MainWindowVm/MainWindowViewModel.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Linq;
+using System;
+using System.Linq.Expressions;
+using System.Windows;
 using System.Windows.Input;
+using TeploenergoSchedule.Infrastructure.Commands;
 using TeploenergoSchedule.Model;
 using TeploenergoSchedule.Model.AppSettings.AppConfig;
 using TeploenergoSchedule.Service.UserDialogService;
 using TeploenergoSchedule.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace TeploenergoSchedule.ViewModels.MainWindowVm
 {
@@ -11,12 +17,21 @@ namespace TeploenergoSchedule.ViewModels.MainWindowVm
     {
         private readonly IAppConfig _appConfig;
         private readonly IUserDialogService _userDialogService;
+
+        private string[] _fileNames;
+
         /* ------------------------------------------------------------------------------------------------------------ */
         public MainWindowViewModel(IUserDialogService userDialogService)
         {
             _log.Debug($"Вызов конструктора {GetType().Name}");
             _appConfig = AppConfig.GetConfigFromDefaultPath();
             _userDialogService = userDialogService;
+
+            FileNames = new ObservableCollection<string>();
+
+            #region Commands
+            LoadFiles = new RelayCommand(OnLoadFilesExecuted, CanLoadFilesExecute);
+            #endregion
         }
 
         /// <summary>
@@ -29,6 +44,38 @@ namespace TeploenergoSchedule.ViewModels.MainWindowVm
         /* ------------------------------------------------------------------------------------------------------------ */
         #region Commands
 
+        #region LoadFiles
+        public ICommand LoadFiles { get; }
+        private void OnLoadFilesExecuted(object p)
+        {
+            try
+            {
+                var ofd = new OpenFileDialog();
+
+                //ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+                ofd.Filter = "xlsx files|*.xlsx";
+                ofd.RestoreDirectory = true;
+                ofd.Multiselect = true;
+
+                if (ofd.ShowDialog() != true)
+                    return;
+                
+                _fileNames = ofd.FileNames;
+                FileNames.Clear();
+                foreach(string file in _fileNames)
+                {
+                    FileNames.Add(file);    
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+            }
+        }
+
+        private bool CanLoadFilesExecute(object p) => true;
+        #endregion
+
         #region Exit
         public ICommand Exit { get; }
         private void OnExitExecuted(object p) => Application.Current.Shutdown();
@@ -38,6 +85,9 @@ namespace TeploenergoSchedule.ViewModels.MainWindowVm
         #endregion
 
         /* ------------------------------------------------------------------------------------------------------------ */
+
+        public ObservableCollection<string> FileNames { get; set; }
+
         #region Window title
 
         private string _title = $"{AppConst.Get().AppDesciption} {ProjectVersion.Get()}";
